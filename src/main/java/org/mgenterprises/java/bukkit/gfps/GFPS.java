0e2a3666ec12d8cba687ae2e0459772dec940425
MANUEL <MANUEL@DESKTOP1-WIN8>
2013-02-10 19:26:27 -0500

    Try to fix mystery error

--- src/main/java/org/mgenterprises/java/bukkit/gfps/GFPS.java
+++ src/main/java/org/mgenterprises/java/bukkit/gfps/GFPS.java
@@ -16,21 +16,25 @@ import org.mgenterprises.java.bukkit.gmcfps.Core.Teams.Team;
  */
 public class GFPS extends JavaPlugin {
 
+    @Override
     public void onEnable() {
         Game g = new Game("test");
         FPSCore core = new FPSCore(g);
-        core.getTeamManager().registerTeam(new Team("Team1"));
-        core.getTeamManager().registerTeam(new Team("Team2"));
+        Team t1 =new Team("Team1");
+        Team t2 = new Team("Team2");
+        core.getTeamManager().registerTeam(t1);
+        core.getTeamManager().registerTeam(t2);
         GameManager gm = new GameManager(this);
         gm.registerGame(g);
         GameManagementCommands gmcommand = new GameManagementCommands(gm);
         getCommand("join").setExecutor(gmcommand);
 
-        
+
         this.getServer().getPluginManager().registerEvents(core.getCombatListener(), this);
         this.getServer().getPluginManager().registerEvents(core.getWeaponListeners(), this);
     }
 
+    @Override
     public void onDisable() {
         getLogger().info("onDisable has been invoked!");
     }
